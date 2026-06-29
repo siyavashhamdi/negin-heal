aa4f43d790ca2867872f58e953e7025957d7f42e
Siya <siyavash.hamdi@cultofcoders.com>
2026-06-29 19:11:29 +0330

    chore: removed extra migrations

--- api/src/migrations/index.ts
+++ api/src/migrations/index.ts
@@ -6,7 +6,5 @@
 // Import migrations in order (they will be sorted by version automatically)
 export * from "./001-create-default-super-admin-users.migration";
 export * from "./002-seed-default-app-settings.migration";
-export * from "./003-rename-course-collections-to-product.migration";
-export * from "./004-repair-course-to-product-collection-rename.migration";
 
 // Add more migrations here as you create them:
